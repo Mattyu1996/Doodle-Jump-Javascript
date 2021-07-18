8f534c500a4492a41b0db79bf7e7ad21d3e93617
Mattyu1996 <mattia.lenza@student.univaq.it>
2021-07-18 02:35:58 +0200

    Added shoe spawning system

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -5,6 +5,7 @@ import DoodleRJ from '../assets/img/doodle_rj.png';
 import DoodleRS from '../assets/img/doodle_rs.png';
 import DoodleLJ from '../assets/img/doodle_lj.png';
 import DoodleLS from '../assets/img/doodle_ls.png';
+import Spring from '../assets/img/spring_h.png';
 
 function GameScreen(){
     let screen = new Phaser.Scene('game');
@@ -18,11 +19,14 @@ function GameScreen(){
         this.load.image('doodle_rs', DoodleRS);
         this.load.image('doodle_lj', DoodleLJ);
         this.load.image('doodle_ls', DoodleLS);
+        this.load.image('spring', Spring);
     }
 
     screen.create = function(){
+        this.keyboardArrows = this.input.keyboard.createCursorKeys();
         this.camera = this.cameras.cameras[0];
         this.physics.world.setBounds(0, 0, this.sys.canvas.height, this.sys.canvas.width);
+        
         this.background = this.add.sprite(0, 0, 'background').setOrigin(0,0);
         this.background.displayWidth = this.sys.canvas.width;
         this.background.displayHeight = vh(110);
@@ -44,7 +48,8 @@ function GameScreen(){
         this.camera.startFollow(this.doodle);
         this.camera.setLerp(0,1);
         
-        this.keyboardArrows = this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(this.platforms, this.shoes);
+        this.physics.add.overlap(this.doodle, this.shoes, collectShoes, null, this);
     }
 
     screen.update = function (){
@@ -78,15 +83,15 @@ function GameScreen(){
         
         for (let platform of this.platforms.children.entries) {
             if(platform.y >= (this.camera.worldView.y+this.sys.canvas.height+vh(2.5))){
-                console.log('Rimuovo');
                 this.platforms.remove(platform);
                 platform.destroy();
-                makePlatform(getRandomInt(vw(10), vw(90)), this.camera.worldView.top-vh(10));
+                makePlatform(getRandomInt(vw(10), vw(90)), this.camera.worldView.top-vh(9));
             }   
         }
     }
 
     var makePlatforms = (function(){
+        makeShoes();
         this.platforms = this.physics.add.staticGroup();
         this.platforms.enableBody = true;
         this.platforms.createMultiple(10, 'platform');
@@ -100,7 +105,7 @@ function GameScreen(){
     }).bind(screen);
 
     var makePlatform = (function (x, y, width, height){
-        var platform = this.platforms.create(x,y, 'platform');
+        let platform = this.platforms.create(x,y, 'platform');
         platform.displayWidth = vw(20);
         platform.displayHeight = vh(5);
         platform.scaleY = 2;
@@ -109,10 +114,17 @@ function GameScreen(){
         platform.body.immovable = true;
         platform.body.allowGravity = false;
         this.platforms.add(platform);
+        console.log(this.shoes.children.entries)
+        if(this.score > 30000 && this.shoes.children.entries.length < 1 && this.score%30000 == 0){
+            let shoe = this.shoes.create((platform.x+(platform.width/2)-vw(7)), (platform.y+platform.height-vh(6.5)), 'spring');
+            shoe.displayWidth = vw(10);
+            shoe.displayHeight = vh(5);
+            shoe.scaleX = 1;
+            shoe.scaleY = 1;
+        }
     }).bind(screen);
 
     var jump = (async function(){
-        //console.log(this.doodle)
         this.doodle.body.velocity.y = -4000;
         if(this.doodle.texture.key == 'doodle_rj'){
             this.doodle.setTexture('doodle_rs');
@@ -128,14 +140,30 @@ function GameScreen(){
 
 
     var right = (function(){
-        this.doodle.setTexture('doodle_rj');
+        if(this.doodle.texture.key != 'doodle_rj' || this.doodle.texture.key != 'doodle_rs') {
+            this.doodle.setTexture('doodle_rj');
+        }
+
         this.doodle.body.velocity.x = 1200;
     }).bind(screen);
 
     var left = (function(){
-        this.doodle.setTexture('doodle_lj');
+        if(this.doodle.texture.key != 'doodle_lj' || this.doodle.texture.key != 'doodle_ls'){
+            this.doodle.setTexture('doodle_lj');
+        } 
+        
         this.doodle.body.velocity.x = -1200;
     }).bind(screen);
+
+    var makeShoes = (function(){
+        this.shoes = this.physics.add.staticGroup();
+        this.shoes.createMultiple(1, 'spring');
+    }).bind(screen);
+
+    var collectShoes = (function(doodle, shoe){
+        shoe.disableBody(true,true);
+    }).bind(screen);
+
     return screen;
 }
 
